05e13d02aa2305f84143c4092a1d45ae6dc512ed
Anthony Xu <anthonyyxu.work@gmail.com>
2025-03-26 14:25:44 -0500

    added spring boot template code for backend

---
+++ backend/src/main/java/com/team48/procompare/ProcompareApplication.java
@@ -0,0 +1,13 @@
+package com.team48.procompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcompareApplication.class, args);
+	}
+
+}
