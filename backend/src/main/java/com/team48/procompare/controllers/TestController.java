273968592d21faa99ca6fc7753d6209a84e7d8ee
Anthony Xu <anthonyyxu.work@gmail.com>
2025-03-30 17:54:37 -0500

    fixed dependencies, added test connection to gcloud db

--- backend/src/main/java/com/team48/procompare/controllers/TestController.java
+++ backend/src/main/java/com/team48/procompare/controllers/TestController.java
@@ -2,10 +2,22 @@ package com.team48.procompare.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 @RestController
 public class TestController {
-    @GetMapping("/test")
-    public String test() {
-        return "Hello, World!";
+    private final JdbcTemplate jdbcTemplate;
+
+    public TestController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @GetMapping("/getUsers")
+    public List<String> getTuples() {
+        return this.jdbcTemplate.queryForList("SELECT * FROM Users LIMIT 5").stream()
+                .map(m -> m.values().toString())
+                .collect(Collectors.toList());
     }
 }
